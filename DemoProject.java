c8c3c1b8671cb3f365217ed9d9c23a783236d4f6
Parameswari410 <65904561+Parameswari410@users.noreply.github.com>
2020-07-09 18:56:09 +0530

    Initial commit

---
+++ DemoProject.java
@@ -0,0 +1,10 @@
+
+public class DemoProject {
+
+	public static void main(String[] args) {
+		System.out.println("There's Hope");
+		System.out.println("Miracle will happen");
+		stem.out.println("Believe in Universe");
+	}
+
+}
