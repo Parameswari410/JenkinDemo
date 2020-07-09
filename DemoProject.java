edb3b94fcb1e16ed47966ebcc61a3b3ae67dbbe0
Parameswari410 <65904561+Parameswari410@users.noreply.github.com>
2020-07-09 19:22:12 +0530

    update spelling

--- DemoProject.java
+++ DemoProject.java
@@ -4,6 +4,6 @@ public class DemoProject {
 	public static void main(String[] args) {
 		System.out.println("There's Hope");
 		System.out.println("Miracle will happen");
-		stem.out.println("Believe in Universe");
+		System.out.println("Believe in Universe");
 	}
 }
